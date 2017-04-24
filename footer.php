572bee7926ee20de7d1f84de4688f0e3b180e3da
Diego Versiani <diegoversiani@gmail.com>
2017-04-24 21:50:14 +0200

    Change footer style and general typography.

--- footer.php
+++ footer.php
@@ -54,7 +54,7 @@
         <div class="site-info">
           <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'split' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'split' ), 'WordPress' ); ?></a>
           <span class="sep"> | </span>
-          <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'split' ), __( 'Split' , 'split'), __( '<a href="http://getunderskeleton.com" rel="designer">getunderskeleton.com</a>' , 'split') ); ?>
+          <?php printf( esc_html__( 'Theme: %1$s by %2$s', 'split' ), __( 'Split' , 'split'), __( '<a href="http://diegoversiani.me" rel="designer">Diego Versiani</a>' , 'split') ); ?>
         </div><!-- .site-info -->
       </div><!-- .row -->
 
--- inc/widgets-areas.php
+++ inc/widgets-areas.php
@@ -25,67 +25,6 @@ add_action( 'widgets_init', 'split_widgets_init' );
 
 
 
-
-
-/*------------------------------------*\
-  #HOMEPAGE WIDGET AREAS
-\*------------------------------------*/
-
-function split_homepage_widgets_init() {
-
-  /* #HOMEPAGE BEFORE CONTENT */
-    
-  register_sidebar( array(
-    'name'          => __( 'Homepage Before Content (DEPRECATED)', 'split' ),
-    'id'            => 'split_homepage_before',
-    'description'   => __( 'Insert widgets before the content in the homepage template.', 'split' ),
-    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h2>',
-    'after_title'   => '</h2>',
-  ) );
-
-
-  /* #HOMEPAGE AFTER CONTENT */
-
-  register_sidebar( array(
-    'name'          => __( 'Homepage After Content (DEPRECATED)', 'split' ),
-    'id'            => 'split_homepage_after',
-    'description'   => __( 'Insert widgets after the content in the homepage template.', 'split' ),
-    'before_widget' => '<div id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</div>',
-    'before_title'  => '<h2>',
-    'after_title'   => '</h2>',
-  ) );
-
-}
-add_action( 'widgets_init', 'split_homepage_widgets_init', 0);
-
-
-
-
-
-/*------------------------------------*\
-  #POSTS LISTS WIDGET AREAS
-\*------------------------------------*/
-
-function split_posts_lists_widgets_init() {
-
-  register_sidebar( array(
-    'name'          => __( 'Posts Lists (DEPRECATED)', 'split' ),
-    'id'            => 'split_posts_lists',
-    'description'   => __( 'Insert widgets after posts lists such as the blog page and archives.', 'split' ),
-    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-    'after_widget'  => '</aside>',
-    'before_title'  => '<h5>',
-    'after_title'   => '</h5>',
-  ) );
-
-}
-add_action( 'widgets_init', 'split_posts_lists_widgets_init', 0);
-
-
-
 /*------------------------------------*\
   #SHOP WIDGET AREAS
 \*------------------------------------*/
@@ -116,7 +55,7 @@ function split_footer_widgets_init() {
     'description'   => __( 'Widgets for one column footer.', 'split' ),
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h4>',
+    'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ) );
 
@@ -128,7 +67,7 @@ function split_footer_widgets_init() {
     'description'   => __( 'Widgets for left column on three columns footer.', 'split' ),
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h4>',
+    'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ) );
 
@@ -138,7 +77,7 @@ function split_footer_widgets_init() {
     'description'   => __( 'Widgets for center column on three columns footer.', 'split' ),
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h4>',
+    'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ) );
 
@@ -148,7 +87,7 @@ function split_footer_widgets_init() {
     'description'   => __( 'Widgets for right column on three columns footer.', 'split' ),
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
-    'before_title'  => '<h4>',
+    'before_title'  => '<h4 class="widget-title">',
     'after_title'   => '</h4>',
   ) );
 
